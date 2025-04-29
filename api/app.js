676b5ed0eb50910e3ca96faea81df52a90ff2f26
Niharika Swain <swainniharika27@gmail.com>
2025-04-29 11:40:23 +0530

    uploaded

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoute from "./Routes/postRoutes.js";
 import authRoute from "./Routes/authRoutes.js";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -26,20 +27,32 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async(req, res) => {
+export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
     //Checking If The User Exists
     const user = await prisma.user.findUnique({
-        where: {username}
-    })
+      where: { username },
+    });
 
-    if(!user) return status(404).json({message: "User Not Found"})
+    if (!user) return res.status(401).json({ message: "Invalid Credentials" });
 
-    //Checking If The Passwor Is Correct
+    //Checking If The Password Is Correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials" });
 
     //Generate Cookie Token And Send To The User
+
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000 * 60 * 60 * 24 * 7
+    res.cookie("test2", "myValue2", {
+      httpOnly: true,
+      // secure: true,
+      maxAge: age,
+    }).status(200).json({message: "Login Successful"})
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error" });
