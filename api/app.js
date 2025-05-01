69d7ca6452dbaf444d56aea789f747980cee8eee
Niharika Swain <swainniharika27@gmail.com>
2025-05-01 21:27:53 +0530

    uploaded

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./Routes/postRoutes.js";
 import authRoute from "./Routes/authRoutes.js";
 
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
